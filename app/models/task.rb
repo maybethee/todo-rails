13cb01fd3e2a1067448c5d1984c53c6053291e2d
retra <jozalamun@gmail.com>
2024-05-15 08:12:32 -0400

    Add Edit actions and render form in both new and edit views

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,8 @@
 class Task < ApplicationRecord
   validates :title, presence: true
-  
+
   belongs_to :user
   has_many :task_categories
   has_many :categories, through: :task_categories
+  accepts_nested_attributes_for :task_categories
 end
--- app/models/task_category.rb
+++ app/models/task_category.rb
@@ -1,4 +1,5 @@
 class TaskCategory < ApplicationRecord
   belongs_to :task
   belongs_to :category
+  accepts_nested_attributes_for :categories
 end
