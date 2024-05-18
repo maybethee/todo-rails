549ce8de603705c3ab7972e2939663395d75c92e
retra <jozalamun@gmail.com>
2024-05-18 11:48:55 -0400

    Add dropdown to filter displayed tasks by their assigned category

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,12 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @tasks = @user.tasks
+
+    # need to amend this to accept a no_category option or something
+    if params[:name].present?
+      @tasks = @user.tasks.joins(:categories).where(categories: { name: params[:name] })
+    else
+      @tasks = @user.tasks
+    end
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -4,6 +4,6 @@ class Task < ApplicationRecord
   belongs_to :user
   # must include inverse_of when submitting a POST form using a has_many through: relation apparently
   has_many :task_categories, inverse_of: :task
-  has_many :categories, through: :task_categories
+  has_many :categories, through: :task_categories, dependent: :destroy
   accepts_nested_attributes_for :task_categories
 end
